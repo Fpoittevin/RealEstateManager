e18c1108807e4bc8be95bc86c27a8c495ac0b540
Fpoittevin <dev@francoispoittevin.Fr>
2020-08-06 10:45:13 +0200

    add map and markers in map view activity

--- app/src/main/java/com/ocr/francois/realestatemanager/ui/mapView/MapViewActivity.kt
+++ app/src/main/java/com/ocr/francois/realestatemanager/ui/mapView/MapViewActivity.kt
@@ -1,12 +1,61 @@
 package com.ocr.francois.realestatemanager.ui.mapView
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.ocr.francois.realestatemanager.R
+import com.ocr.francois.realestatemanager.injection.Injection
+import com.ocr.francois.realestatemanager.viewmodels.PropertyViewModel
 
-class MapViewActivity : AppCompatActivity() {
+
+class MapViewActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private val propertyViewModel: PropertyViewModel by viewModels() {
+        Injection.provideViewModelFactory(this)
+    }
+    private lateinit var map: GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map_view)
+
+        configureMap()
+
+    }
+
+    private fun configureMap() {
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.activity_map_view_map_container) as SupportMapFragment?
+        mapFragment!!.getMapAsync(this)
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        this.map = map
+        getAllProperties()
+    }
+
+    private fun addMarker(lat: Double, lng: Double) {
+        val latLng = LatLng(lat, lng)
+        val markerOptions = MarkerOptions().position(latLng)
+        map.addMarker(markerOptions)
+        map.moveCamera(CameraUpdateFactory.newLatLng(latLng))
+        map.moveCamera(CameraUpdateFactory.zoomTo(15F))
+    }
+
+    private fun getAllProperties() {
+        propertyViewModel.getAllProperties().observe(this, Observer { properties ->
+            for (property in properties) {
+                if (property.lat != null && property.lng != null) {
+                    addMarker(property.lat!!, property.lng!!)
+                }
+            }
+        })
     }
 }
